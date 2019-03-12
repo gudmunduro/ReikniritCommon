a7c1d56534002cfbff10fdcfcad91050a324f268
gudmunduro <goh140400@gmail.com>
2019-03-12 12:02:55 +0000

    Updated common.swift

--- Sources/Common/common.swift
+++ Sources/Common/common.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 // Functions
 
@@ -36,6 +37,54 @@ public func ask(_ message: String = "") -> Bool
     }
 }
 
+func deg(_ value: Double) -> Double
+{
+    return value * 180 / Double.pi
+}
+
+func deg(_ value: Float) -> Float
+{
+    return value * 180 / Float.pi
+}
+
+enum GraphPosition {
+    case PP
+    case NP
+    case NN
+    case PN
+
+    static func find(x: Double, y: Double) -> GraphPosition
+    {
+        if x >= 0 && y > 0 {
+            return .PP
+        } else if x < 0 && y > 0 {
+            return .NP
+        } else if x < 0 && y <= 0 {
+            return .NN
+        } else if x >= 0 && y <= 0 {
+            return .PN
+        }
+        return .PP
+    }
+}
+
+// Function overloads
+
+public func sqrt(_ value: Int) -> Int
+{
+    return Int(sqrt(Double(value)))
+}
+
+public func acos(_ value: Int) -> Int
+{
+    return Int(acos(Double(value)))
+}
+
+public func pow(_ value: Int, _ power: Int) -> Int 
+{
+    return Int(pow(Double(value), Double(power)))
+}
+
 // Classes
 
 public class Menu {
@@ -57,7 +106,7 @@ public class Menu {
     func show()
     {
         print(title)
-        for i in 0...options.count - 1 {
+        for i in 0..<options.count {
             print("\(i + 1)) \(options[i].text)")
         }
         while true {
@@ -84,10 +133,18 @@ public class Menu {
 
 extension Int {
 
-    static postfix func ++(lhs: inout Int) -> Int {
+    static postfix func ++ (lhs: inout Int) -> Int {
         lhs += 1
         return lhs
     }
+}
+
+extension Double {
+
+    func rounded(decimals: Int) -> Double {
+        return (self * pow(10.0, Double(decimals))).rounded() / pow(10.0, Double(decimals))
+    }
+
 }
 
 extension String {
@@ -107,15 +164,13 @@ extension String {
 
 extension Array {
 
-    mutating func swap(_ ind1: Int, _ ind2: Int){
-        var temp: Element
-        temp = self[ind1]
+    mutating func swap(_ ind1: Int, _ ind2: Int) {
+        let temp = self[ind1]
         self[ind1] = self[ind2]
         self[ind2] = temp
     }
 
-    func printAll()
-    {
+    func printAll() {
         for element in self {
             print(element)
         }
